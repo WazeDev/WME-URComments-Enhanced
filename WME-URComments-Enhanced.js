87dadbe4ed86100127878509709a3a8a0f7a0f26
dBsooner <daniel@dbsooner.com>
2023-04-05 12:46:08 -0500

    Update WME-URComments-Enhanced.js
    Production version.

--- WME-URComments-Enhanced.js
+++ WME-URComments-Enhanced.js
@@ -1,5 +1,5 @@
 // ==UserScript==
-// @name        WME URComments-Enhanced (beta)
+// @name        WME URComments-Enhanced
 // @namespace   https://greasyfork.org/users/166843
 // @version     2023.04.05.01
 // eslint-disable-next-line max-len
